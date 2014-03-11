6bf5c3e10bc730495094e4898f784a346bd6562b
Rodrigo Dias <rod@LDM-Vader.local>
2014-03-11 17:13:35 +0000

    deleting a few plugins and adding forms and a new theme

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,10 @@
  * @package WordPress
  */
 
+define( 'FS_METHOD', 'direct' );
+//define( 'FS_CHMOD_DIR', 0777 );
+//define( 'FS_CHMOD_FILE', 0777 );
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'enclothed');
