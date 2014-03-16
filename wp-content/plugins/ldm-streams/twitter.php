be657eec9c8afc2c3c2c780983acc24189059d9c
Rodrigo Dias <rodrigo.pitta@gmail.com>
2014-03-16 20:45:04 +0000

    adding ldm streams module from which 3 shortags should come up

--- wp-content/plugins/ldm-streams/twitter.php
+++ wp-content/plugins/ldm-streams/twitter.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) {
   $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $oauth_token, $oauth_token_secret);
   return $connection;
