5c3642231a0583e80110fe0c6f2e8321e0103bcc
Louis <slickmin2007@hotmail.co.uk>
2014-03-19 16:45:03 +0000

    New fonts added

--- wp-content/themes/enclothed/templates/header.php
+++ wp-content/themes/enclothed/templates/header.php
@@ -1,14 +1,23 @@
 <header class="banner container" role="banner">
   <div class="row">
-    <div class="col-lg-12">
-      <a class="brand" href="<?php echo home_url('/') ?>"><?php bloginfo('name'); ?></a>
-      <nav class="nav-main" role="navigation">
-        <?php
-          if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills'));
-          endif;
-        ?>
-      </nav>
-    </div>
-  </div>
+    <div id="logo"></div>
+    <div class="navbar-header">
+			<!-- BEGIN RESPONSIVE MENU TOGGLER -->
+				<button class="navbar-toggle btn navbar-btn" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<!-- END RESPONSIVE MENU TOGGLER -->
+	</div>
+	<!-- BEGIN TOP NAVIGATION MENU -->
+		<div class="navbar-collapse collapse">
+				<?php
+                  if (has_nav_menu('primary_navigation')) :
+                    wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+                  endif;
+                ?>
+		</div>
+	<!-- BEGIN TOP NAVIGATION MENU -->
+  </div> 
 </header>
