a38cca6da571b4850d700d9efdca067db0fdc8f3
Louis <slickmin2007@hotmail.co.uk>
2014-03-19 17:07:51 +0000

    Header style changes

--- wp-content/themes/enclothed/templates/header.php
+++ wp-content/themes/enclothed/templates/header.php
@@ -1,23 +1,30 @@
-<header class="banner container" role="banner">
-  <div class="row">
-    <div id="logo"></div>
-    <div class="navbar-header">
-			<!-- BEGIN RESPONSIVE MENU TOGGLER -->
+<div class="header navbar navbar-default navbar-static-top">
+<div class="container">
+			<div class="navbar-header">
+				<!-- BEGIN RESPONSIVE MENU TOGGLER -->
 				<button class="navbar-toggle btn navbar-btn" data-toggle="collapse" data-target=".navbar-collapse">
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-			<!-- END RESPONSIVE MENU TOGGLER -->
-	</div>
-	<!-- BEGIN TOP NAVIGATION MENU -->
-		<div class="navbar-collapse collapse">
+				<!-- END RESPONSIVE MENU TOGGLER -->
+				<!-- BEGIN LOGO (you can use logo image instead of text)-->
+				<a class="navbar-brand logo-v1" href="index.html">
+					<img src="<?php bloginfo('template_directory'); ?>/assets/img/logo-black.png" id="logoimg" alt="">
+				</a>
+				<!-- END LOGO -->
+			</div>
+		
+			<!-- BEGIN TOP NAVIGATION MENU -->
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
 				<?php
                   if (has_nav_menu('primary_navigation')) :
                     wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
                   endif;
-                ?>
+                ?>	
+				</ul>                           
+			</div>
+			<!-- BEGIN TOP NAVIGATION MENU -->
 		</div>
-	<!-- BEGIN TOP NAVIGATION MENU -->
-  </div> 
-</header>
+</div>
