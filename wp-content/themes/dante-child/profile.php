99b4b967833d4e9c789d4597e225938735281b81
Rodrigo Dias <rodrigo@likedigitalmedia.com>
2014-03-21 13:18:34 +0000

    fixing redirect issues on profile

--- wp-content/themes/dante-child/profile.php
+++ wp-content/themes/dante-child/profile.php
@@ -4,12 +4,13 @@
 * template name: Profile 
 *
 **/
-?>
-
-
-<?php //if the user us logged in he does not need to enrol again. Maybe he just wants another box ?>
-<?php if ( is_user_logged_in() ) :?>
-	<?php get_header(); ?>
+ //if the user us logged in he does not need to enrol again. Maybe he just wants another box 
+if ( !is_user_logged_in() ) :
+	wp_redirect(get_uri()."/address/"); 
+	exit;
+ //if the user is not logged in then bring him to the registration form 
+else : 
+get_header(); ?>
 	<h2>Dashboard</h2>
 	
 	<div><a href="/profile/address">Edit your Profile</a></div>
@@ -32,9 +33,6 @@
 		<input type="submit" value="submit">
 	</form>
 	<?php get_footer(); ?>
-<?php //if the user is not logged in then bring him to the registration form ?>
-<?php else : ?>
-	<?php wp_redirect(get_uri()."/address/") ?>
 <?php endif; ?>
 
 
