e525f19dfd44c8d722fdef3fff505217a78c2b7f
Louis <slickmin2007@hotmail.co.uk>
2014-03-21 17:36:59 +0000

    Profile details page

--- wp-content/themes/dante-child/profile-details.php
+++ wp-content/themes/dante-child/profile-details.php
@@ -21,10 +21,9 @@ if (isset($_SESSION['section_1'])){
 
 
 <div class="inner-page-wrap has-no-sidebar clearfix">
-	<div class="post-8 page type-page status-publish hentry clearfix" id="8">				
 		<div class="page-content clearfix">
 			<div class="row">
-                <div class="full-width-text spb_content_element col-sm-12 no-padding-top spb_text_column">
+                <div class="full-width-text spb_content_element col-sm-12 spb_text_column" style="padding-top:30px;">
                     <div class="spb_wrapper clearfix">
                         <h1 style="text-align: center;"><span style="color: #ffffff;">BUILD YOUR<br>
                         ENCLOTHED PROFILE</span></h1>
@@ -42,39 +41,29 @@ if (isset($_SESSION['section_1'])){
  			<li><a href="http:/#/">Price and Summary</a></li>
  			<li><a href="http:/#/">Delivery</a></li>
  		</ul>
- <div class="shadow"><img src="img/shadow.png" alt="" /></div>
+ <div class="shadow"><img src="<?php bloginfo('template_url') ?>-child/images/shadow.png" alt="" /></div>
  </div><!--details-menu-->
   
  	<div class="styles-block">
  	
+    <form action="" method="POST" name='section_1'>
+        <?php $nonce = wp_create_nonce( get_uri() ); ?>
+            <div class="flashmessages"><?php flashMessagesDisplay(); ?></div>
 			<div class="title-forms">
 				<div class="mini-wrapper-forms">
 		 	  	<div class="numbering">01</div>
 		 	  	<p>Some Key Pieces of information we need about you.</p>
-		 	  	<input type="text" class="key-info" tabindex="1" value="Full Name" required onfocus="if(this.value==this.defaultValue)this.value=''"    
-		 	  	onblur="if(this.value=='')this.value=this.defaultValue">
-		 	  	<input type="text" class="key-info" tabindex="1" value="Post Code" required
-		 	  	onfocus="if(this.value==this.defaultValue)this.value=''"    
-		 	  	onblur="if(this.value=='')this.value=this.defaultValue">
-		 	  	<input type="text" class="key-info" tabindex="1" value="Address" required
-		 	  	onfocus="if(this.value==this.defaultValue)this.value=''"    
-		 	  	onblur="if(this.value=='')this.value=this.defaultValue">
-		 	  	<input type="text" class="key-info" tabindex="1" value="Phone Number" required
-		 	  	onfocus="if(this.value==this.defaultValue)this.value=''"    
-		 	  	onblur="if(this.value=='')this.value=this.defaultValue">
-		 	  	<input type="town" class="key-info" tabindex="1" value="Town/City" required
-		 	  	onfocus="if(this.value==this.defaultValue)this.value=''"    
-		 	  	onblur="if(this.value=='')this.value=this.defaultValue">
-		 	  	<input type="occupation" class="key-info" tabindex="1" value="Occupation" required
-		 	  	onfocus="if(this.value==this.defaultValue)this.value=''"    
-		 	  	onblur="if(this.value=='')this.value=this.defaultValue">
-		 	  	<input type="email" class="key-info" tabindex="1" value="EMail Address" required
-		 	  	onfocus="if(this.value==this.defaultValue)this.value=''"    
-		 	  	onblur="if(this.value=='')this.value=this.defaultValue">
-		 	  	<input type="password" class="key-info" tabindex="2" placeholder="Password" required>
+		 	  	<input type="text" class="key-info" tabindex="1" name="section_1[name]" placeholder='Full Name' value="<?php @echo_if_exists($section['name']); ?>">
+		 	  	<input type="text" class="key-info" tabindex="2" name="section_1[address]" placeholder='Address' value="<?php @echo_if_exists($section['address']); ?>">
+		 	  	<input type="text" class="key-info" tabindex="3" name="section_1[city]" placeholder='Town / City' value="<?php @echo_if_exists($section['city']); ?>">
+		 	  	<input type="text" class="key-info" tabindex="4" name="section_1[email]" placeholder='Email' value="<?php @echo_if_exists($section['email']); ?>">
+		 	  	<input type="text" class="key-info" tabindex="5" name="section_1[post_code]" placeholder='Post Code' value="<?php @echo_if_exists($section['post_code']); ?>">
+		 	  	<input type="text" class="key-info" tabindex="6" name="section_1[phone]" placeholder='Phone Number' value="<?php @echo_if_exists($section['phone']); ?>">
+		 	  	<input type="text" class="key-info" tabindex="7" name="section_1[occupation]" placeholder='Occupation' value="<?php @echo_if_exists($section['occupation']); ?>">
+		 	  	<input type="password" class="key-info"  name="section_1[password]" placeholder='Password' value="<?php @echo_if_exists($section['password']); ?>">
 
-			<select class="selectmenu">
-  				<option value="Date of Birth">Date of Birth</option>
+			<select class="selectmenu" tabindex="9" name='section_1[dob]' placeholder='Date of Birth' value="<?php @echo_if_exists($section['dob']); ?>">
+				<option value="0">Date of Birth</option>
   				<option value="Select date">Select date</option>
   				<option value="Select date">Select date</option>
   				<option value="Select date">Select date</option>
@@ -84,7 +73,7 @@ if (isset($_SESSION['section_1'])){
 			(Click in the box for date picker, or click again just to type it.)
 			</span>
 			
-			<select class="selectmenu">
+			<select class="selectmenu" tabindex="10" name="section_1[feedback_1]">
 					<option value="Date of Birth">How did you hear about enclothed?</option>
 					<option value="The Internet">The Internet</option>
 					<option value="Word of Mouth">Word of Mouth</option>
@@ -96,7 +85,7 @@ if (isset($_SESSION['section_1'])){
 					<option value="Other">Other</option>
 			</select>
 			
-			<select class="selectmenu">
+			<select class="selectmenu" tabindex="11" name="section_1[feedback_2]">
 					<option value="Date of Birth">Are you purchasing for another person?</option>
 					<option value="Yes">Yes</option>
 					<option value="No">No</option>
@@ -105,63 +94,16 @@ if (isset($_SESSION['section_1'])){
 		 	  	</div><!--mini-wrapper-forms-->
 		 	  	
 		  <div class="mini-wrapper5">
-		  	<a href="http:/#/" class="button4">Save and continue</a>
+            		<input type="hidden" value="<?php echo $nonce; ?>" name='nonce'>
+                <button class="button4" onclick="submit()">Save and Continue</button>
 		 </div><!--mini-wrapper4-->
 		 
- </div><!--styles-block-->
+                 </div>
+                </form>
+                 <!--styles-block-->
             </div>            
 		</div>
-	</div>
 </div>
-
-
-
-
-
-
-
-
-<h3><?php the_title(); ?></h3> 
-<form action="" method="POST" name='section_1'>
-	<?php $nonce = wp_create_nonce( get_uri() ); ?>
-	<div class="flashmessages"><?php flashMessagesDisplay(); ?></div>
-
-	<div style="float:left;">
-		<input type="text" name="section_1[name]" placeholder='Full Name' value="<?php @echo_if_exists($section['name']); ?>"><br>
-		<input type="text" name="section_1[address]" placeholder='Address' value="<?php @echo_if_exists($section['address']); ?>"><br>
-		<input type="text" name="section_1[city]" placeholder='Town / City' value="<?php @echo_if_exists($section['city']); ?>"><br>
-		<input type="text" name="section_1[email]" placeholder='Email' value="<?php @echo_if_exists($section['email']); ?>"><br>
-	</div>
-	
-	<div style="float:left; padding-left:30px;">
-		<input type="text" name="section_1[post_code]" placeholder='Post Code' value="<?php @echo_if_exists($section['post_code']); ?>"><br>
-		<input type="text" name="section_1[phone]" placeholder='Phone Number' value="<?php @echo_if_exists($section['phone']); ?>"><br>
-		<input type="text" name="section_1[occupation]" placeholder='Occupation' value="<?php @echo_if_exists($section['occupation']); ?>"><br>
-		<input type="password" name="section_1[password]" placeholder='Password' value="<?php @echo_if_exists($section['password']); ?>"><br>
-	</div>
-
-	<br><br><br><br><br><br>
-
-	<div>
-		<input type="text" name='section_1[dob]' placeholder='Date of Birth' value="<?php @echo_if_exists($section['dob']); ?>">
-
-		<h5>How did you hear about Enclothed</h5>
-		<select name="section_1[feedback_1]">
-			<option value="0">Select One</option>
-			<option value="word">Word of Mouth</option>
-			<option value="Magazine">Magazine</option>
-			<option value="other">Other</option>
-		</select>
-		<input type="text" name="section_1[feedback_2]" placeholder='What other...'>
-	</div>
-	
-
-	<br><br><br><br><br><br>
-
-
-	<input type="hidden" value="<?php echo $nonce; ?>" name='nonce'>
-	<button onclick="submit()">Save and Continue</button>
-</form>
 
 
 <?php get_footer(); ?>
