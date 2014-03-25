b77c40e2918e5c176caf5248b323c432c4c24f21
Louis <slickmin2007@hotmail.co.uk>
2014-03-25 15:54:28 +0000

    Fixed updated theme styles

--- wp-content/themes/dante-child/header.php
+++ wp-content/themes/dante-child/header.php
@@ -19,6 +19,10 @@
 			$enable_top_bar = $options['enable_tb'];
 			$enable_mini_header = $options['enable_mini_header'];
 			$enable_header_shadow = $options['enable_header_shadow'];
+			$header_search_type = "search-1";
+			if (isset($options['header_search_type'])) {
+				$header_search_type = $options['header_search_type'];
+			}
 			
 			$page_class = $header_wrap_class = $logo_class = $ss_enable = "";
 			
@@ -30,13 +34,17 @@
 				$header_wrap_class = " container";
 				$page_class .= "header-overlay ";
 			}
+
+			if (isset($options['enable_fw_header']) && $options['enable_fw_header'] == true) {
+				$header_wrap_class .= " fw-header";
+			}
 			
 			global $sf_catalog_mode;
 			if (isset($options['enable_catalog_mode'])) {
 				$enable_catalog_mode = $options['enable_catalog_mode'];
 				if ($enable_catalog_mode) {
 					$sf_catalog_mode = true;
-					$page_class = "catalog-mode ";
+					$page_class .= "catalog-mode ";
 				}
 			}
 			
@@ -68,10 +76,12 @@
 				$ss_enable = true;
 			}
 			
-			global $post;
+			global $post, $remove_promo_bar, $enable_one_page_nav;
 			$extra_page_class = $description = "";
 			if ($post) {
-			$extra_page_class = get_post_meta($post->ID, 'sf_extra_page_class', true);
+				$extra_page_class = get_post_meta($post->ID, 'sf_extra_page_class', true);
+				$remove_promo_bar = get_post_meta($post->ID, 'sf_remove_promo_bar', true);
+				$enable_one_page_nav = get_post_meta($post->ID, 'sf_enable_one_page_nav', true);
 			}
 		?>
 		
@@ -103,7 +113,7 @@
 		<?php } ?>
 				
 		<?php
-			$custom_fonts = $google_font_one = $google_font_two = $google_font_three = "";
+			$custom_fonts = $google_font_one = $google_font_two = $google_font_three = $google_font_subset = $subset_output = "";
 
 			$body_font_option = $options['body_font_option'];
 			if (isset($options['google_standard_font'])) {
@@ -111,21 +121,39 @@
 			}
 			$headings_font_option = $options['headings_font_option'];
 			if (isset($options['google_heading_font'])) {
-			$google_font_two = $options['headings_font_option'];
+			$google_font_two = $options['google_heading_font'];
 			}
 			$menu_font_option = $options['menu_font_option'];
 			if (isset($options['google_menu_font'])) {
 			$google_font_three = $options['google_menu_font'];
-			}			
+			}
+			
+			if (isset($options['google_font_subset'])) {
+			$google_font_subset = $options['google_font_subset'];
+				$s = 0;
+				if (is_array($google_font_subset)) {
+					foreach ($google_font_subset as $subset) {
+						if ($subset == "none") {
+							break;
+						}
+						if ($s > 0) {
+						$subset_output .= ','.$subset;
+						} else {
+						$subset_output = ':'.$subset;
+						}
+						$s++;
+					}
+				}
+			}
 			    
 			if ($body_font_option == "google" && $google_font_one != "") {
-				$custom_fonts .= "'".$google_font_one."', ";
+				$custom_fonts .= "'".$google_font_one.$subset_output."', ";
 			}
 			if ($headings_font_option == "google" && $google_font_two != "") {
-				$custom_fonts .= "'".$google_font_two."', ";
+				$custom_fonts .= "'".$google_font_two.$subset_output."', ";
 			}
 			if ($menu_font_option == "google" && $google_font_three != "") {
-				$custom_fonts .= "'".$google_font_three."', ";
+				$custom_fonts .= "'".$google_font_three.$subset_output."', ";
 			}
 			
 			$fontdeck_js = $options['fontdeck_js'];
@@ -171,12 +199,7 @@
 	</head>
 	
 	<!--// OPEN BODY //-->
-	<body <?php body_class($page_class.' '.$is_responsive.' '.$extra_page_class); ?> ontouchstart="">
-		
-		<!--// NO JS ALERT //-->
-		<noscript>
-			<div class="no-js-alert"><?php _e("Please enable JavaScript to view this website.", "swiftframework"); ?></div>
-		</noscript>
+	<body <?php body_class($page_class.' '.$is_responsive.' '.$extra_page_class.' '.$header_search_type); ?> ontouchstart="">
 		
 		<div id="header-search">
 			<div class="container clearfix">
@@ -188,10 +211,8 @@
 		
 		<?php
 			// SUPER SEARCH
-			if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-				if ($ss_enable) { 
-					echo sf_super_search();
-				}
+			if (sf_woocommerce_activated() && $ss_enable) { 
+				echo sf_super_search();
 			}
 		?>
 		
@@ -223,7 +244,8 @@
 			
 			<!--// OPEN #main-container //-->
 			<div id="main-container" class="clearfix relative">
-				<div class="page_bottom_bg"></div>
+								<div class="page_bottom_bg"></div>
+				
 				<?php if (is_page()) {
 					global $post;
 					$show_posts_slider = get_post_meta($post->ID, 'sf_posts_slider', true);
@@ -234,7 +256,9 @@
 						sf_swift_slider();
 					} else if ($rev_slider_alias != "") { ?>
 						<div class="home-slider-wrap">
-							<?php putRevSlider($rev_slider_alias); ?>
+							<?php if (function_exists('putRevSlider')) {
+								putRevSlider($rev_slider_alias);
+							} ?>
 						</div>
 				<?php } else if ($layerSlider_ID != "") { ?>
 					<div class="home-slider-wrap">
@@ -243,9 +267,6 @@
 				<?php }
 					}
 				?>
-				
-				<!--// OPEN .container //-->
-				<div class="container">
 								
-					<!--// OPEN #page-wrap //-->
-					<div id="page-wrap">
+				<!--// OPEN #page-wrap //-->
+				<div id="page-wrap">
