ba4cf7767e138411c7f0bd0510a62d7e9618b113
Louis <slickmin2007@hotmail.co.uk>
2014-03-25 17:11:25 +0000

    Pricing and profile page changes

--- wp-content/themes/dante-child/profile-delivery.php
+++ wp-content/themes/dante-child/profile-delivery.php
@@ -4,20 +4,132 @@
 * template name: Profile Delivery
 *
 **/
+?>
+<?php get_header(); ?>
 
-get_header();
-if (isset($_SESSION['section_2'])){
-	$section = $_SESSION['section_2'];	
-}else if(isset($_POST['section_2'])){
-	$section = $_POST['section_2'];
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.9.1.js"></script>
+<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<link rel="stylesheet" href="/resources/demos/style.css">
+<script>
+$(document).ready(function(){
+	$( "#datepicker" ).datepicker( $.datepicker.regional[ "uk" ] );
+	$( "#datepicker" ).datepicker( "option", "dateFormat", 'dd-mm-yy' );
+	$( "#datepicker" ).datepicker();
+});
+
+</script>
+
+<?php 
+if (isset($_SESSION['section_1'])){
+	$section = $_SESSION['section_1'];	
+}else if(isset($_POST['section_1'])){
+	$section = $_POST['section_1'];
 } else {
 	$section = array();
 }
 
+
+?>
+	
+<?php
+	$options = get_option('sf_dante_options');
+	
+	$default_show_page_heading = $options['default_show_page_heading'];
+	$default_page_heading_bg_alt = $options['default_page_heading_bg_alt'];
+	$default_sidebar_config = $options['default_sidebar_config'];
+	$default_left_sidebar = $options['default_left_sidebar'];
+	$default_right_sidebar = $options['default_right_sidebar'];
+	
+	$pb_active = get_post_meta($post->ID, '_spb_js_status', true);
+	$show_page_title = get_post_meta($post->ID, 'sf_page_title', true);
+	$page_title_style = get_post_meta($post->ID, 'sf_page_title_style', true);
+	$page_title = get_post_meta($post->ID, 'sf_page_title_one', true);
+	$page_subtitle = get_post_meta($post->ID, 'sf_page_subtitle', true);
+	$page_title_bg = get_post_meta($post->ID, 'sf_page_title_bg', true);
+	$fancy_title_image = rwmb_meta('sf_page_title_image', 'type=image&size=full');
+	$page_title_text_style = get_post_meta($post->ID, 'sf_page_title_text_style', true);
+	$fancy_title_image_url = "";
+	
+	if ($show_page_title == "") {
+		$show_page_title = $default_show_page_heading;
+	}
+	if ($page_title_bg == "") {
+		$page_title_bg = $default_page_heading_bg_alt;
+	}
+	if ($page_title == "") {
+		$page_title = get_the_title();
+	}
+	
+	foreach ($fancy_title_image as $detail_image) {
+		$fancy_title_image_url = $detail_image['url'];
+		break;
+	}
+									
+	if (!$fancy_title_image) {
+		$fancy_title_image = get_post_thumbnail_id();
+		$fancy_title_image_url = wp_get_attachment_url( $fancy_title_image, 'full' );
+	}
+	
+	$sidebar_config = get_post_meta($post->ID, 'sf_sidebar_config', true);
+	$left_sidebar = get_post_meta($post->ID, 'sf_left_sidebar', true);
+	$right_sidebar = get_post_meta($post->ID, 'sf_right_sidebar', true);
+	
+	if ($sidebar_config == "") {
+		$sidebar_config = $default_sidebar_config;
+	}
+	if ($left_sidebar == "") {
+		$left_sidebar = $default_left_sidebar;
+	}
+	if ($right_sidebar == "") {
+		$right_sidebar = $default_right_sidebar;
+	}
+	
+	sf_set_sidebar_global($sidebar_config);
+	
+	$page_wrap_class = $post_class_extra = '';
+	if ($sidebar_config == "left-sidebar") {
+	$page_wrap_class = 'has-left-sidebar has-one-sidebar row';
+	$post_class_extra = 'col-sm-8';
+	} else if ($sidebar_config == "right-sidebar") {
+	$page_wrap_class = 'has-right-sidebar has-one-sidebar row';
+	$post_class_extra = 'col-sm-8';
+	} else if ($sidebar_config == "both-sidebars") {
+	$page_wrap_class = 'has-both-sidebars row';
+	$post_class_extra = 'col-sm-9';
+	} else {
+	$page_wrap_class = 'has-no-sidebar';
+	}
+	
+	$remove_breadcrumbs = get_post_meta($post->ID, 'sf_no_breadcrumbs', true);
+	$remove_bottom_spacing = get_post_meta($post->ID, 'sf_no_bottom_spacing', true);
+	$remove_top_spacing = get_post_meta($post->ID, 'sf_no_top_spacing', true);
+	
+	if ($remove_bottom_spacing) {
+	$page_wrap_class .= ' no-bottom-spacing';
+	}
+	if ($remove_top_spacing) {
+	$page_wrap_class .= ' no-top-spacing';
+	}
+	
+	$options = get_option('sf_dante_options');
+	$disable_pagecomments = false;
+	if (isset($options['disable_pagecomments']) && $options['disable_pagecomments'] == 1) {
+	$disable_pagecomments = true;
+	}
 ?>
 
 
-<div class="inner-page-wrap has-no-sidebar clearfix">
+<?php if ($sidebar_config != "no-sidebars" || $pb_active != "true") { ?>
+<div class="container">
+<?php } ?>
+
+<div class="inner-page-wrap <?php echo $page_wrap_class; ?> clearfix">
+		
+	<?php if (have_posts()) : the_post(); ?>
+
+	<!-- OPEN page -->
+	<div <?php post_class('clearfix ' . $post_class_extra); ?> id="<?php the_ID(); ?>">
 		<div class="page-content clearfix">
 			<div class="row">
                 <div class="full-width-text spb_content_element col-sm-12 spb_text_column no-padding-top">
@@ -55,7 +167,7 @@ if (isset($_SESSION['section_2'])){
 				<div class="mini-wrapper-forms">
 		 	  	<input type="text" class="customer-info" tabindex="1" placeholder="Customer Address Line 1" name="" value="">		 	  	
 		 	  	<input type="text" class="customer-info" tabindex="1" placeholder="Customer Address Line 2" name="" value="">
-		 	  	<input type="town" class="key-info" tabindex="1" placeholder="Town/City" name="" value="">
+		 	  	<input type="text" class="key-info" tabindex="1" placeholder="Town/City" name="" value="">
 		 	  	<input type="text" class="key-info" tabindex="1" placeholder="Post Code" name="" value="">		 	  	
 		 	  	<input type="text" class="customer-info2" tabindex="1" placeholder="Name this Address" name="" value="">
 		 	  	
@@ -71,10 +183,10 @@ if (isset($_SESSION['section_2'])){
 				<div class="mini-wrapper-forms">
 		 	  	<input type="text" class="customer-info" tabindex="1" placeholder="Alternative Address Line 1" name="" value="">		 	  	
 		 	  	<input type="text" class="customer-info" tabindex="1" placeholder="Alternative Address Line 2" name="" value="">	  	
-		 	  	<input type="town" class="key-info" tabindex="1" placeholder="Alternative Town/City" name="" value="">
+		 	  	<input type="text" class="key-info" tabindex="1" placeholder="Alternative Town/City" name="" value="">
 		 	  	<input type="text" class="key-info" tabindex="1" placeholder="Alternative Post Code" name="" value="">
 		 	  	<input type="text" class="customer-info" tabindex="2" placeholder="Name this Address" name="" value="">
-				<div style="width: 90%;max-width: 637px;height: 55px; margin:30px auto 50px auto;">
+				<div class="checkbox_wrap" >
                 	<input type="checkbox" class="css-checkbox" id="checkbox1">
                 	<label for="checkbox1" name="checkbox1_lbl" class="css-label tickbox">- MY BILLING ADDRESS IS THE SAME AS MY DEFAULT ADDRESS</label>
 				</div>
@@ -93,7 +205,7 @@ if (isset($_SESSION['section_2'])){
 			<div class="mini-wrapper-forms">
 	 	  	<input type="text" class="customer-info" tabindex="1" placeholder="Billing Address Line 1" name="" value="">
 	 	  	<input type="text" class="customer-info" tabindex="1" placeholder="Billing Address Line 2" name="" value="">
-	 	  	<input type="town" class="key-info" tabindex="1" placeholder="Billing Town/City" name="" value="">
+	 	  	<input type="text" class="key-info" tabindex="1" placeholder="Billing Town/City" name="" value="">
 	 	  	<input type="text" class="key-info" tabindex="1" placeholder="Billing Post Code" name="" value="">
 	 	  	<input type="text" class="customer-info2" tabindex="2" placeholder="Name this Address" name="" value="">
             
@@ -118,4 +230,17 @@ if (isset($_SESSION['section_2'])){
 </div>
 
 </div>
+	<!-- CLOSE page -->
+
+	<?php endif; ?>
+	
+
+</div>
+<?php if ($sidebar_config != "no-sidebars" || $pb_active != "true") { ?>
+</div>
+<?php } ?>
+
+<!--// WordPress Hook //-->
 <?php get_footer(); ?>
+
+
--- wp-content/themes/dante-child/profile-details.php
+++ wp-content/themes/dante-child/profile-details.php
@@ -4,8 +4,8 @@
 * template name: Profile Details
 *
 **/
-
-get_header(); ?>
+?>
+<?php get_header(); ?>
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 <script src="//code.jquery.com/jquery-1.9.1.js"></script>
@@ -31,9 +31,106 @@ if (isset($_SESSION['section_1'])){
 
 
 ?>
+	
+<?php
+	$options = get_option('sf_dante_options');
+	
+	$default_show_page_heading = $options['default_show_page_heading'];
+	$default_page_heading_bg_alt = $options['default_page_heading_bg_alt'];
+	$default_sidebar_config = $options['default_sidebar_config'];
+	$default_left_sidebar = $options['default_left_sidebar'];
+	$default_right_sidebar = $options['default_right_sidebar'];
+	
+	$pb_active = get_post_meta($post->ID, '_spb_js_status', true);
+	$show_page_title = get_post_meta($post->ID, 'sf_page_title', true);
+	$page_title_style = get_post_meta($post->ID, 'sf_page_title_style', true);
+	$page_title = get_post_meta($post->ID, 'sf_page_title_one', true);
+	$page_subtitle = get_post_meta($post->ID, 'sf_page_subtitle', true);
+	$page_title_bg = get_post_meta($post->ID, 'sf_page_title_bg', true);
+	$fancy_title_image = rwmb_meta('sf_page_title_image', 'type=image&size=full');
+	$page_title_text_style = get_post_meta($post->ID, 'sf_page_title_text_style', true);
+	$fancy_title_image_url = "";
+	
+	if ($show_page_title == "") {
+		$show_page_title = $default_show_page_heading;
+	}
+	if ($page_title_bg == "") {
+		$page_title_bg = $default_page_heading_bg_alt;
+	}
+	if ($page_title == "") {
+		$page_title = get_the_title();
+	}
+	
+	foreach ($fancy_title_image as $detail_image) {
+		$fancy_title_image_url = $detail_image['url'];
+		break;
+	}
+									
+	if (!$fancy_title_image) {
+		$fancy_title_image = get_post_thumbnail_id();
+		$fancy_title_image_url = wp_get_attachment_url( $fancy_title_image, 'full' );
+	}
+	
+	$sidebar_config = get_post_meta($post->ID, 'sf_sidebar_config', true);
+	$left_sidebar = get_post_meta($post->ID, 'sf_left_sidebar', true);
+	$right_sidebar = get_post_meta($post->ID, 'sf_right_sidebar', true);
+	
+	if ($sidebar_config == "") {
+		$sidebar_config = $default_sidebar_config;
+	}
+	if ($left_sidebar == "") {
+		$left_sidebar = $default_left_sidebar;
+	}
+	if ($right_sidebar == "") {
+		$right_sidebar = $default_right_sidebar;
+	}
+	
+	sf_set_sidebar_global($sidebar_config);
+	
+	$page_wrap_class = $post_class_extra = '';
+	if ($sidebar_config == "left-sidebar") {
+	$page_wrap_class = 'has-left-sidebar has-one-sidebar row';
+	$post_class_extra = 'col-sm-8';
+	} else if ($sidebar_config == "right-sidebar") {
+	$page_wrap_class = 'has-right-sidebar has-one-sidebar row';
+	$post_class_extra = 'col-sm-8';
+	} else if ($sidebar_config == "both-sidebars") {
+	$page_wrap_class = 'has-both-sidebars row';
+	$post_class_extra = 'col-sm-9';
+	} else {
+	$page_wrap_class = 'has-no-sidebar';
+	}
+	
+	$remove_breadcrumbs = get_post_meta($post->ID, 'sf_no_breadcrumbs', true);
+	$remove_bottom_spacing = get_post_meta($post->ID, 'sf_no_bottom_spacing', true);
+	$remove_top_spacing = get_post_meta($post->ID, 'sf_no_top_spacing', true);
+	
+	if ($remove_bottom_spacing) {
+	$page_wrap_class .= ' no-bottom-spacing';
+	}
+	if ($remove_top_spacing) {
+	$page_wrap_class .= ' no-top-spacing';
+	}
+	
+	$options = get_option('sf_dante_options');
+	$disable_pagecomments = false;
+	if (isset($options['disable_pagecomments']) && $options['disable_pagecomments'] == 1) {
+	$disable_pagecomments = true;
+	}
+?>
 
 
-<div class="inner-page-wrap has-no-sidebar clearfix">
+<?php if ($sidebar_config != "no-sidebars" || $pb_active != "true") { ?>
+<div class="container">
+<?php } ?>
+
+<div class="inner-page-wrap <?php echo $page_wrap_class; ?> clearfix">
+		
+	<?php if (have_posts()) : the_post(); ?>
+
+	<!-- OPEN page -->
+	<div <?php post_class('clearfix ' . $post_class_extra); ?> id="<?php the_ID(); ?>">
+	
 		<div class="page-content clearfix">
 			<div class="row">
                 <div class="full-width-text spb_content_element col-sm-12 spb_text_column no-padding-top">
@@ -116,5 +213,21 @@ if (isset($_SESSION['section_1'])){
 		</div>
 </div>
 
+
+	
+	<!-- CLOSE page -->
+	</div>
+
+	<?php endif; ?>
+	
+
 </div>
+
+<?php if ($sidebar_config != "no-sidebars" || $pb_active != "true") { ?>
+</div>
+<?php } ?>
+
+<!--// WordPress Hook //-->
 <?php get_footer(); ?>
+
+
