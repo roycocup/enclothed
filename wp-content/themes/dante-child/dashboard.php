310966af3b4dabb0ffd041e592ba0bc76e55c7cb
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2014-04-02 17:57:32 +0100

    a bit better dashboard

--- wp-content/themes/dante-child/dashboard.php
+++ wp-content/themes/dante-child/dashboard.php
@@ -19,16 +19,31 @@ get_header(); ?>
 		<form action="" method="POST" name='more_box'>
 		<?php $nonce = wp_create_nonce( get_uri() ); ?>
 			<div class="flashmessages"><?php flashMessagesDisplay(); ?></div>
-			<select name="more_box[address]" id="more_box[addresses]">
-				<option value="1">Address 1 - delivery default</option>
-				<option value="2">Address 2</option>
-				<option value="3">Address 3</option>
-			</select>
+			
+			<div class="row">
+				<div class="col-xs-12">
+					<select name="more_box[address]" id="more_box[addresses]">
+						<option value="1">Address 1 - delivery default</option>
+						<option value="2">Address 2</option>
+						<option value="3">Address 3</option>
+					</select>
+				</div>
+				<br><br>
+
+				<div class="col-xs-6">
+					<input type="text" placeholder='Promotional Code' name='promocode'>	
+				</div>
+
+				<div class="col-xs-6">
+					<input type="text" placeholder='Gift Card Code' name='giftcode'>
+				</div>
+			</div>
 			<br><br>
-			<input type="text" placeholder='Promotional Code' name='promocode'> <input type="text" placeholder='Gift Card Code' name='giftcode'>
-			<br><br>
+
 			<input type="hidden" value="<?php echo $nonce; ?>" name='nonce'>
-			<input type="submit" value="submit">
+			<div class="mini-wrapper5">
+                <button class="button4" onclick="submit()">Save</button>
+		 	</div>
 		</form>
 	</div>
 	<?php get_footer(); ?>
