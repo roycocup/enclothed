3ed7d72ec63535e6f833f8c54b1bc93be192c31c
Rodrigo Dias <rodrigo.pitta@gmail.com>
2014-04-07 22:14:06 +0100

    Instagram feed - the beginning

--- wp-content/plugins/ldm-streams/ldm-streams.php
+++ wp-content/plugins/ldm-streams/ldm-streams.php
@@ -10,9 +10,7 @@
 *
 **/
 
-// include_once 'facebook3.php';
-//include_once 'twitter.php';
-
+require_once('instagram.class.php');
 
 class ldmStreams {
 
@@ -24,6 +22,22 @@ class ldmStreams {
 	public function __construct(){
 		$this->config = parse_ini_file(dirname(__FILE__)."/".$this->config_filename, true); 
 	}	
+
+
+	public function getInstagram(){
+		$config = array(
+			'apiKey' => $this->config['instagram']['client_id'],
+			'apiSecret' => $this->config['instagram']['client_secret'],
+			'apiCallback' => $this->config['instagram']['redirect_uri'],
+		);
+		return new Instagram($config);
+	}
+
+	public function getInstagramPosts(){
+		$instagram = $this->getInstagram();
+		$posts = $instagram->getUserMedia($this->config['instagram']['enclothed_id']);
+		return $posts; 
+	}
 
 
 }
--- wp-content/themes/dante-child/streams.php
+++ wp-content/themes/dante-child/streams.php
@@ -5,29 +5,9 @@
 *
 **/
 
+$streams = new ldmStreams();
 
-// $posts = get_facebook_posts();
-// $i = 1;
-// foreach ($posts as $post) {
-// 	if ($i >= 2) break;
-// 	if (empty($post['content'])) {
-// 		$post_content = $post['link_description']; 
-// 	} else {
-// 		$post_content = $post['content']; 
-// 	} 
-
-	
-// 	$time = time_elapsed($post['timestamp']);
-
-// 	echo "<div class='fb_post' style='padding-bottom:20px'>";
-// 	echo "<div class='fb_post_content'>$post_content</div>";
-// 	echo "<div class='fb_time'>$time</div>";	
-// 	echo "</div>";
-// 	$i++;
-// }
-
-echo get_fb_posts(2); 
+dump($streams->getInstagramPosts()); 
 
 
-$images = get_fb_post_image(1);
-echo $images;
+
