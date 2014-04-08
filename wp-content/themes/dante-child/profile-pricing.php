64627880c2e7662c39f83e0e7ae82d49b7885251
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2014-04-08 15:37:25 +0100

    styling the placeholders

--- wp-content/themes/dante-child/profile-pricing.php
+++ wp-content/themes/dante-child/profile-pricing.php
@@ -7,6 +7,23 @@
 ?>
 <?php get_header(); ?>
 
+<style>
+	::-webkit-input-placeholder {
+		font-style: italic;
+		font-size: 10px;
+	}
+
+	::-moz-placeholder {  /* Firefox 19+ */
+		font-style: italic;
+		font-size: 10px;
+	}
+
+	:-ms-input-placeholder {  
+		font-style: italic;
+		font-size: 10px;
+	}
+</style>
+
 <script>
 	jQuery(document).ready(function($){
 		
