aa76ae934d70fe72764f3c51f0ef2d47d4ecc753
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2014-04-09 11:09:53 +0100

    hiding the error messages for now

--- wp-content/themes/dante-child/profile-details.php
+++ wp-content/themes/dante-child/profile-details.php
@@ -125,53 +125,53 @@ if (isset($_SESSION['section_1'])){
 	
 		<div class="page-content clearfix">
 			<div class="row">
-                <div class="full-width-text spb_content_element col-sm-12 spb_text_column no-padding-top">
-                    <div class="spb_wrapper clearfix">
-                        <h2 style="text-align: center;"><span style="color: #ffffff;">BUILD YOUR<br>
-                        ENCLOTHED PROFILE</span></h2>
-                        <p style="margin-bottom: 0;"><img class="size-full aligncenter" alt="line" src="<?php echo get_bloginfo('stylesheet_directory') ?>/images/line.png"></p>
+				<div class="full-width-text spb_content_element col-sm-12 spb_text_column no-padding-top">
+					<div class="spb_wrapper clearfix">
+						<h2 style="text-align: center;"><span style="color: #ffffff;">BUILD YOUR<br>
+						ENCLOTHED PROFILE</span></h2>
+						<p style="margin-bottom: 0;"><img class="size-full aligncenter" alt="line" src="<?php echo get_bloginfo('stylesheet_directory') ?>/images/line.png"></p>
 					</div> 
-                </div>
-            </div>
+				</div>
+			</div>
 			<div class="row">
 				<!-- Main Menu -->
- 		<div class="details-menu">
- 		<ul>
- 			<li><span class="active">Your Details</span></li>
+		<div class="details-menu">
+		<ul>
+			<li><span class="active">Your Details</span></li>
 			<li class="hidden-sm hidden-xs"><span>Pick Your Style</span></li>
 			<li class="hidden-sm hidden-xs"><span>Preferences</span></li>
 			<li class="hidden-sm hidden-xs"><span>Size and Color</span></li>
 			<li class="hidden-sm hidden-xs"><span style="border-right:none;">Price and Summary</span></li>
- 		</ul>
+		</ul>
  <div class="shadow"><img src="<?php bloginfo('template_url') ?>-child/images/shadow.png" alt="" /></div>
  </div><!--details-menu-->
   
- 	<div class="styles-block">
- 	<div class="fade-border-left"></div>
- 	<div class="fade-border-right"></div>
-    <form action="" method="POST" name='section_1'>
-        <?php $nonce = wp_create_nonce( get_uri() ); ?>
-            <div class="flashmessages col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
-            	<?php flashMessagesDisplay(); ?>
-            	<p class="error_message">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-            	<ul class="error_fields">
-            		<li>Full Name</li>
-            		<li>Address</li>            		
-            	</ul>
-            </div>
+	<div class="styles-block">
+	<div class="fade-border-left"></div>
+	<div class="fade-border-right"></div>
+	<form action="" method="POST" name='section_1'>
+		<?php $nonce = wp_create_nonce( get_uri() ); ?>
+		<!-- <div class="flashmessages col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+			<?php flashMessagesDisplay(); ?>
+			<p class="error_message">Please correct the following errors</p>
+			<ul class="error_fields">
+				<li>Full Name</li>
+				<li>Address</li>            		
+			</ul>
+		</div> -->
 
 			<div class="title-forms">
 				<div class="mini-wrapper-forms">
-		 	  	<div class="numbering">01</div>
-		 	  	<p>Some Key Pieces of information we need about you.</p>
-		 	  	<input type="text" class="key-info" tabindex="1" name="section_1[name]" placeholder='Full Name' value="<?php @echo_if_exists($section['name']); ?>" >
-		 	  	<input type="text" class="key-info" tabindex="2" name="section_1[address]" placeholder='Address' value="<?php @echo_if_exists($section['address']); ?>">
-		 	  	<input type="text" class="key-info" tabindex="3" name="section_1[city]" placeholder='Town / City' value="<?php @echo_if_exists($section['city']); ?>">
-		 	  	<input type="text" class="key-info" tabindex="4" name="section_1[email]" placeholder='Email' value="<?php @echo_if_exists($section['email']); ?>">
-		 	  	<input type="text" class="key-info" tabindex="5" name="section_1[post_code]" placeholder='Post Code' value="<?php @echo_if_exists($section['post_code']); ?>">
-		 	  	<input type="text" class="key-info" tabindex="6" name="section_1[phone]" placeholder='Phone Number' value="<?php @echo_if_exists($section['phone']); ?>">
-		 	  	<input type="text" class="key-info" tabindex="7" name="section_1[occupation]" placeholder='Occupation' value="<?php @echo_if_exists($section['occupation']); ?>">
-		 	  	<input type="password" class="key-info"  name="section_1[password]" placeholder='Password' value="<?php @echo_if_exists($section['password']); ?>">
+				<div class="numbering">01</div>
+				<p>Some Key Pieces of information we need about you.</p>
+				<input type="text" class="key-info" tabindex="1" name="section_1[name]" placeholder='Full Name' value="<?php @echo_if_exists($section['name']); ?>" >
+				<input type="text" class="key-info" tabindex="2" name="section_1[address]" placeholder='Address' value="<?php @echo_if_exists($section['address']); ?>">
+				<input type="text" class="key-info" tabindex="3" name="section_1[city]" placeholder='Town / City' value="<?php @echo_if_exists($section['city']); ?>">
+				<input type="text" class="key-info" tabindex="4" name="section_1[email]" placeholder='Email' value="<?php @echo_if_exists($section['email']); ?>">
+				<input type="text" class="key-info" tabindex="5" name="section_1[post_code]" placeholder='Post Code' value="<?php @echo_if_exists($section['post_code']); ?>">
+				<input type="text" class="key-info" tabindex="6" name="section_1[phone]" placeholder='Phone Number' value="<?php @echo_if_exists($section['phone']); ?>">
+				<input type="text" class="key-info" tabindex="7" name="section_1[occupation]" placeholder='Occupation' value="<?php @echo_if_exists($section['occupation']); ?>">
+				<input type="password" class="key-info"  name="section_1[password]" placeholder='Password' value="<?php @echo_if_exists($section['password']); ?>">
 
 				<input type='text' class="selectmenu" id='datepicker' tabindex="9" name='section_1[dob]' placeholder='Date of Birth' value="<?php @echo_if_exists($section['dob']); ?>">
 				
@@ -198,18 +198,18 @@ if (isset($_SESSION['section_1'])){
 					<option value="Yes">Yes</option>
 					<option value="No">No</option>
 			</select>
-		 	  	
-		 	  	</div><!--mini-wrapper-forms-->
-		 	  	
+				
+				</div><!--mini-wrapper-forms-->
+				
 		  <div class="mini-wrapper5">
-            		<input type="hidden" value="<?php echo $nonce; ?>" name='nonce'>
-                <button class="button4" onclick="submit()">Save and Continue</button>
+					<input type="hidden" value="<?php echo $nonce; ?>" name='nonce'>
+				<button class="button4" onclick="submit()">Save and Continue</button>
 		 </div><!--mini-wrapper4-->
 		 
-                 </div>
-                </form>
-                 <!--styles-block-->
-            </div>            
+				 </div>
+				</form>
+				 <!--styles-block-->
+			</div>            
 		</div>
 </div>
 
