47d810f67a11563e222699081276d3659ec1606e
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2014-04-10 19:56:07 +0100

    the right page for failure

--- wp-content/plugins/ldm-sagepay/ldm-sagepay.php
+++ wp-content/plugins/ldm-sagepay/ldm-sagepay.php
@@ -35,7 +35,7 @@ class ldm_sagepay {
 		$this->config['billing_country']	= 'GB';
 		$this->config['billing_phone']		= '';
 		$this->config['success_url']		= 'http://enclothed.dev/thank-you';
-		$this->config['failure_url']		= 'http://enclothed.dev/failure';
+		$this->config['failure_url']		= 'http://enclothed.dev/unsuccessful';
 	}
 
 	public function getConfig(){
