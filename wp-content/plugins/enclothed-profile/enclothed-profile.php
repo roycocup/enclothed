8a7b1de68c1608b22b3bc1c095baf0c2a6d52e33
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2014-04-17 16:23:11 +0100

    more data on the reorder email

--- wp-content/plugins/enclothed-profile/enclothed-profile.php
+++ wp-content/plugins/enclothed-profile/enclothed-profile.php
@@ -678,6 +678,8 @@ class EnclothedProfile {
 				} else {
 					$address = $profile->bill_add_1." ".$profile->bill_add_2.", ".$profile->bill_town." ".$profile->bill_post_code;
 				}
+				$data['username'] = $current_user->user_email;
+				$data['user_reference'] = $profile->customer_id;
 				$data['address'] = $address;
 				$this->main->sendmail(get_bloginfo('admin_email'), 'New box requested!', Emails_model::TEMPLATE_NEW_BOX, $data);
 				setFlashMessage('success', 'Your box has been ordered. We shall be in touch soon.'); 
